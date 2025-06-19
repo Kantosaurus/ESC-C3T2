b6cdf54b873cb09796f06cf9ff2c7a16ef16addb
Ryan Tan <tanryan777@gmail.com>
2025-06-19 10:02:51 +0800

    feat: add token expiry flow

--- packages/frontend/src/components/authenticated-route.tsx
+++ packages/frontend/src/components/authenticated-route.tsx
@@ -1,8 +1,10 @@
+import { isTokenValid } from "@/auth/token";
+import { useMemo } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 export const RouteGuard = () => {
-  const token = localStorage.getItem("carely-token");
-  if (token) {
+  // prevent unnecessary re-renders
+  if (useMemo(() => isTokenValid(), [])) {
     return <Outlet />;
   } else {
     return <Navigate to="/login" />;
